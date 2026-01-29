f17763dc6cb9f77b5299ad2c74945390a163e4b0
Daniyal-FS <danial20051225@gmail.com>
2026-01-30 01:27:23 +0500

    Последние поправки скелета

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
-
+const config = useRuntimeConfig()
+const { data } = await useFetch(`${config.public.apiBase}/users`)
 </script>
 
 <template>
   <div class="h-screen flex flex-col items-center justify-center bg-gray-900 gap-4 p-8">
     <h1 class="text-6xl font-bold text-white">안녕하세요 — Hello in Korean</h1>
     <p class="text-xl text-yellow-400 font-medium">Tailwind + Nuxt + Noto Sans KR</p>
+
+    <div v-if="data?.users && data.users.length > 0" class="text-white text-2xl">
+      Hello, {{ data.users[1].username }} — роль: {{ data.users[1].role }}
+    </div>
   </div>
 </template>
-
-
-
-<style scoped>
-
-</style>
